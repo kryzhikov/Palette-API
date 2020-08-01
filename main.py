d572004d1798a3e9219697b701fdb0b970ca52e3
KirillR911 <KirikR911@gmail.com>
2020-08-01 18:32:26 +0300

    Now logging

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, Form,HTTPException
 from pydantic import BaseModel
 from typing import List
 import io
 import numpy as np
 import sys
+import logging
 from model import Prediction
 from Palette import palettedImage
 import json
+import os
+import pandas as pd
 from PIL import Image
+import logging
+
+logger = logging.getLogger('dev')
+logger.setLevel(logging.INFO)
+
+fileHandler = logging.FileHandler('test.log')
+fileHandler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[QUERY] %(asctime)s -  %(image_name)s - %(srv)s - %(message)s')
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+
 
 app = FastAPI()
 
@@ -16,11 +30,12 @@ def root_route():
   return { 'error': 'Use POST /prediction instead of the root route!' }
 
 @app.post('/prediction/', response_model=Prediction)
-async def prediction_route(file: UploadFile = File(...)):
+async def prediction_route(file: UploadFile = File(...), srv : str = Form(...)):
 
   if file.content_type.startswith('image/') is False:
+    extr = {'srv': srv, 'image_name': file.filename}
+    logger.error("ERROR", extra=extr)
     raise HTTPException(status_code=400, detail=f'File \'{file.filename}\' is not an image.')
-
   try:
     contents = await file.read()
     pil_image = Image.open(io.BytesIO(contents))
@@ -30,6 +45,9 @@ async def prediction_route(file: UploadFile = File(...)):
     pim.palettize()
     #pim.save_paletted()
     data = pim.get_params()
+    extr = {'srv' : srv, 'image_name' : file.filename}
+    logger.info("OK", extra=extr)
+
     return {
       'filename'   : file.filename,
       'contenttype': file.content_type,
@@ -37,4 +55,6 @@ async def prediction_route(file: UploadFile = File(...)):
     }
   except:
     e = sys.exc_info()[1]
+    extr = {'srv': srv, 'image_name': None}
+    logger.error(f"ERROR: {str(e)}", extra=extr)
     raise HTTPException(status_code=500, detail=str(e))
